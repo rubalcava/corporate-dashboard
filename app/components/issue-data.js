ddc54a0a4e25bf8c0b7bb905fedb417c8ef458c8
rubalcava <radgokart@gmail.com>
2016-08-29 14:08:06 -0700

    fix: Updated .csv file with correct format per code review fix: Removed JS DOM manipulation in favor of handlebars templating

--- app/components/issue-data.js
+++ app/components/issue-data.js
@@ -4,8 +4,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     runAll: null,
+    csvData: null,
     initTable: function() {
-        function buildTable() {
+            var _component = this;
             var csvFile;
             Ember.$.ajax({
                 url: 'open_issues_data/open_issues_data.csv',
@@ -13,64 +14,35 @@ export default Ember.Component.extend({
                 async: false,
                 success: function(response) {
                     csvFile = response;
+
+                    Papa.parse(csvFile, {
+                        complete: function(results) {
+                            csvFile = results.data;
+
+                            this.csvData = csvFile;
+                            _component.set('issues', this.csvData);
+                        }
+                    });
                 },
                 error: function(err) {
                     console.log(err);
                 }
             });
 
-            Papa.parse(csvFile, {
-                complete: function(results) {
-                    csvFile = results.data;
-                }
-            });
-            var title_row = '';
-            var entire_table = '';
-            for (var i=0; i<csvFile.length; i++) {
-                entire_table = entire_table + '<tr>';
-                /* don't want to add listjs classes to first row*/
-                if (i!== 0) {
-                    for (var j=0; j<csvFile[i].length; j++) {
-                        if (j === 0) {
-                            entire_table = entire_table + '<td class="issue_number">' + csvFile[i][j] + '</td>';
-                        }
-                        else if (j === 1) {
-                            entire_table = entire_table + '<td class="customer_id">' + csvFile[i][j] + '</td>';
-                        }
-                        else if (j === 2) {
-                            entire_table = entire_table + '<td class="date_reported">' + csvFile[i][j] + '</td>';
-                        }
-                        else {
-                            entire_table = entire_table + '<td class="location">' + csvFile[i][j] + '</td>';
-                        }
-                    }
-                }
-                /* first row only */
-                else {
-                    for (var k=0; k<csvFile[i].length; k++) {
-                        title_row = title_row + '<td><strong>' + csvFile[i][k] + '</strong></td>';
-                    }
-                }
-                entire_table = entire_table + '</tr>';
-            }
-            document.getElementById('table-titles-row').innerHTML = title_row;
-            document.getElementById('table-body').innerHTML = entire_table;
-
+            /* TODO uncomment when table figured out
             var options = {
                 valueNames: ["issue_number", "customer_id", "date_reported", "location"]
             };
 
             var myList = new List("table-div", options);
-        }
-        buildTable();
+            */
     }.on('didInsertElement'),
 
     didInsertElement() {
         this.runAll = Ember.run.later(this, function() {
             this.initTable();
-
-            this.runAll = Ember.run.later(this, this.runAll, 60000);
-        }, 60000);
+            this.runAll = Ember.run.later(this, this.runAll, 5000);
+        }, 5000);
     },
 
     didDestroyElement() {
